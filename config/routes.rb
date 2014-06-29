f9d6d4690c7f1ae7ae9c4057de8f12fbd5ebca71
Amy Weiner <arweiner@alumni.stanford.edu>
2014-06-28 20:02:19 -0700

    Style layout for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get 'static_pages/home', to: "static_pages#home", as: 'home'
+  get 'static_pages/contact', to: "static_pages#contact", as: 'contact'
+  get 'static_pages/about', to: "static_pages#about", as: 'about'
 end
