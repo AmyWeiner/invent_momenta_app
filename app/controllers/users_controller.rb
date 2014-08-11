14ffef6a1ecb565e114f80df47902182cc562107
Amy Weiner <arweiner@alumni.stanford.edu>
2014-08-11 15:52:20 -0700

    Add funcitonality to allow user to edit account settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Account settings updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
