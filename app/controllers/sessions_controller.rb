af8f9dc13ffc2ed048c5cb4cd1995f4871fb2874
Amy Weiner <arweiner@alumni.stanford.edu>
2014-08-11 19:17:18 -0700

    Add functionality for friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # sign user in and redirect to user's profile page
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination' 
       render 'new'
     end
   end
